7441fe0ef8ddaa01ea18870ed6ee83a122d75917
Joseph Herrera <tony.herrera90@gmail.com>
2020-09-05 12:26:45 -0500

    second addition

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -1,11 +1,52 @@
 package com.skilldistillery.foodproject;
 
-public class FoodTruck	
+public class FoodTruck {
+	private double numericId;
+	private String name;
+	private String foodType;
+	private double numericRating;
 
-	{
-		int numericId;
-		String name;
-		String foodType;
-		int numericRating;
+	public FoodTruck(double numericId, String name, String foodType, double numericRating) {
+	}
+
+	public FoodTruck(Double numericId, String name, String foodType, double numericRating) {
+
+		this.numericId = numericId;
+		this.name = name;
+		this.foodType = foodType;
+		this.numericRating = numericRating;
+	}
+
+	public double getNumericId() {
+		return numericId;
+	}
+
+	public void setNumericId(double numericId) {
+		this.numericId = numericId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getNumericRating() {
+		return numericRating;
+	}
+
+	public void setNumericRating(double numericRating) {
+		this.numericRating = numericRating;
+	}
 
 }
--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -1,9 +1,27 @@
 package com.skilldistillery.foodproject;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
+	private FoodTruck[] FoodTrucks;
+	private int numOfFoodTrucks = 5;
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		FoodTruck[] myFoodTruckSelection;
+		myFoodTruckSelection = new FoodTruck[5];
+
+		System.out.println("Please enter your desired foodtruck: ");
+		String userTruck = sc.nextLine();
+		
+		System.out.println("Please enter your choice foods: ");
+		String userFood = sc.nextLine();
+		
+		System.out.println("Please enter your rating: ");
+		Double userRating = sc.nextDouble();
+
+	
+		sc.close();
 	}
-
 }
